0b1c9a4366bea63f2c7f2bfcc084cd7ab82875f4
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-06-06 17:30:43 -0500

    Finalized the webgen script. Added some hyphens in filenames, made an index doc.

--- makewiki.sh
+++ makewiki.sh
@@ -1,19 +1,20 @@
 # makewiki.sh
 # Lillian Lemmer
-# 2015-06-04T18:03
 #
 # ZSH script for publishing a heirarchy of asciidoc files to web format.
 #
 # Tested in FreeBSD.
 
 SOURCE="websource"
-DESTINATION="."
 
-for relative_adoc_path in "${SOURCE}"/**.adoc
+rm *.html
+cd "${SOURCE}"
+
+for relative_adoc_path in **.adoc
 do
     relative_adoc_dirname=$(dirname "${relative_adoc_path}")
-    mkdir -p "${DESTINATION}/${relative_adoc_dirname}"
+    mkdir -p "${relative_adoc_dirname}"
     adoc_name_no_ext=$(basename "${relative_adoc_path}" .adoc)
-    html_file_path="${DESTINATION}/${relative_adoc_dirname}/${adoc_name_no_ext}.html"
+    html_file_path="../${relative_adoc_dirname}/${adoc_name_no_ext}.html"
     asciidoc "-o" "${html_file_path}" "-a" "toc2" "${relative_adoc_path}"
 done

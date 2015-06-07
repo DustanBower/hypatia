40e104aad3b9653b76c575c7b5da8a0f58f800e5
Lillian Lemmer <lillian.lynn.lemmer@gmail.com>
2015-06-07 01:09:38 -0500

    Files over 2,000 bytes will be generated with the "-n" option (enumerated sections).

--- mksite.sh
+++ mksite.sh
@@ -16,5 +16,13 @@ do
     adoc_name_no_ext=$(basename "${relative_adoc_path}" .adoc)
     html_file_path="../${relative_adoc_dirname}/${adoc_name_no_ext}.html"
     rm "${html_file_path}"
-    asciidoc "-o" "${html_file_path}" "-a" "toc2" "${relative_adoc_path}"
+
+    if [ $(stat -f %z "${relative_adoc_path}") -gt 2000 ]
+    then
+        # file is larger than 2kb, better use enumerated sections
+        asciidoc "-n" "-o" "${html_file_path}" "-a" "toc2" "${relative_adoc_path}"
+    else
+        asciidoc "-o" "${html_file_path}" "-a" "toc2" "${relative_adoc_path}"
+    fi
+    echo
 done
